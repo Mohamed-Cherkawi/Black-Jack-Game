98b90d27cac1633273ee7e32c47d387918a9bb3a
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-10-13 22:54:36 +0100

    Refactored Code ...

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        String chosenOption;
         /* *************************** Variables ***************************** */
-        Game game = new Game(); // Creating A New Game
-        game.createCards(); // Calling The Function That Gnerate Cards And Storing Them To The CardsList by For eching in each enum
+        String chosenOption;
         /* *************************** Game ***************************** */
         System.out.println("<3 % £> $ &> * §> ========================================= Black Jack Game ======================================= <3 % £> $ &> * §> \n");
         System.out.println("$|-->> Enter The [Start] Method To Begin A New Game");
         System.out.println("$|-->> Enter [Leave] To Leave Applicaton");
         System.out.print("\n \t Please Enter Your Choice Here : ");
         chosenOption = ValidationMet.stringScannerMeth((byte) 0);
-        if(chosenOption.equals("start")) game.LaunchingNewGame();
+        if(chosenOption.equals("start")) {
+            Game game = new Game(); // Creating A New Game
+            game.createCards(); // Calling The Function That Generate Cards And Storing Them To The CardsList by For Looping in each enum
+            game.LaunchingNewGame();
+        }
         System.out.println("<3 % £> $ &> * §> ========================================= Left Black Jack Game ======================================= <3 % £> $ &> * §>");
     }
-
 }
