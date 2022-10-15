39b17ef228185e8f7b36c51e8e86e642f768e2a5
Mohamed-Cherkawi <emailforstudy911@gmail.com>
2022-10-15 23:08:14 +0100

    Refactored Code , Removed Useless Code

--- src/Value.java
+++ src/Value.java
@@ -1,24 +1,22 @@
 public enum Value {
-    LAS("Las",(byte) 1 , (byte) 1 ),// Can hold Two values 1 and 11 but by default i give it 1
-    DOS("Dos", (byte) 2, (byte) 2) ,
-    TRES("Tres", (byte) 3 , (byte) 3) ,
-    CUATRO("Cuatro", (byte) 4 , (byte) 4) ,
-    CINCO("Cinco", (byte) 5 , (byte) 5) ,
-    SEIS("Seis", (byte) 6 , (byte) 6) ,
-    SIETE("Siete", (byte) 7 , (byte) 7) ,
-    OCHO("Ocho", (byte) 8 , (byte) 8) ,
-    NUEVO("Nuevo", (byte) 9 , (byte) 9) ,
-    DIEZ("Diez", (byte) 10 , (byte) 10) ,
-    JACK("Jack", (byte) 11 , (byte) 10),
-    QUEEN("Queen", (byte) 12 , (byte) 10),
-    KING("King", (byte) 13 , (byte) 10);
+    LAS("Las", (byte) 1 ),// Can hold Two values 1 and 11 but by default i give it 1
+    DOS("Dos", (byte) 2) ,
+    TRES("Tres",  (byte) 3) ,
+    CUATRO("Cuatro",  (byte) 4) ,
+    CINCO("Cinco",  (byte) 5) ,
+    SEIS("Seis",  (byte) 6) ,
+    SIETE("Siete",  (byte) 7) ,
+    OCHO("Ocho",  (byte) 8) ,
+    NUEVO("Nuevo",  (byte) 9) ,
+    DIEZ("Diez", (byte) 10) ,
+    JACK("Jack",  (byte) 10),
+    QUEEN("Queen",  (byte) 10),
+    KING("King",  (byte) 10);
     private final String name;
-    private final byte number;
     private final byte value;
 
-    private Value(String name, byte number , byte value) {
+    private Value(String name , byte value) {
         this.name = name;
-        this.number = number;
         this.value = value;
     }
 
@@ -30,7 +28,4 @@ public enum Value {
         return value;
     }
 
-    public byte getCardNum() {
-        return number;
-    }
 }
